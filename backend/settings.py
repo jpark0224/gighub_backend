f3d8e16de1ed23fe969521d68237a37c6e0cbee1
Julie Park <juliepark242@gmail.com>
2022-05-12 01:07:14 +0100

    finalise deployment

--- backend/settings.py
+++ backend/settings.py
@@ -14,19 +14,29 @@ import os
 from datetime import timedelta
 from pathlib import Path
 import dj_database_url
+import environ
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Set the project base directory
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Take environment variables from .env file
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m&jt&l%3*zi5oxugstl13gvhn1ndzfbaj)leg%dm4za0l9jsvr'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = ["gighub1.herokuapp.com",
                  "localhost", "127.0.0.1"]
