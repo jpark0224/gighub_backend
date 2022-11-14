81b6b8f8aa3385e95012311a55ebbc75b3b35301
Julie Park <juliepark242@gmail.com>
2022-11-14 22:25:34 +0000

    set up aws

--- backend/settings.py
+++ backend/settings.py
@@ -173,7 +173,7 @@ AWS_S3_CUSTOM_DOMAIN = 'd3io2mpnufpriy.cloudfront.net'
 
 AWS_LOCATION = 'static'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-DEFAULT_FILE_STORAGE = 'backend.storage_backends.MediaStorage' #the media storage configurations
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 
